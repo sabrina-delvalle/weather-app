9d5748672600216915b6fc77e7339876446c0bd6
Sabrina <is.sabrinadelvalle@gmail.com>
2022-06-02 22:37:05 -0400

    Center Weather Info Places

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './Component/NavBar';
 import Search from './Component/Search';
+import Weather from './Component/Weather';
 
 function App() {
   return (<div>     
     <NavBar />
     <Search />
+    <Weather />
   </div>
   );
 }
---
+++ src/Component/Weather.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class Weather extends Component {
+  render() {
+    return <div className="Weather-Info">
+                <div className="weather-op">INFO here there are tons of inresting imgs...</div>
+                <div className="weather-op">Just a graphic</div>
+            </div>;
+  }
+}
+
+export default Weather;
